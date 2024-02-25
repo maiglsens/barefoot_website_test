696eab15e6d207ae34316da0a3ef4084873a3556
detlefmahr <dmahr@simport.de>
2024-02-25 11:56:03 +0100

    add new .js file for dsgvo conform google maps widget

---
+++ assets/js/my_own.js
@@ -0,0 +1,5 @@
+function block() {
+    document.getElementById("map").src = "https://www.google.com/maps/d/embed?mid=1kZ5oOpi9301C1odVvrTIcOTw7FUUhlk&ehbc=2E312F&noprof=1";
+    document.getElementById("map_img").style.display = "none";
+    document.getElementById("map").style.display = "block";
+}
